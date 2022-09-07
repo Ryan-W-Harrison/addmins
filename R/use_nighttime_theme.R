8616db44bdb9f2e46a11a44ab1381765baea57f7
Ryan-W-Harrison <harrison.ryan.w@gmail.com>
2022-09-07 12:53:05 -0400

    Change dark theme to Idle Fingers

--- R/use_nighttime_theme.R
+++ R/use_nighttime_theme.R
@@ -1,12 +1,13 @@
 #' Use nighttime theme.
 #'
-#' Quickly switch to nighttime theme, currently this is Solarized Dark.
+#' Quickly switch to nighttime theme, currently this is Idle Fingers.
 #'
 #' @export
 
 use_nighttime_theme <- function(){
 
   # Set default theme
-  rstudioapi::applyTheme("Solarized Dark")
+  # rstudioapi::applyTheme("Solarized Dark")
+  rstudioapi::applyTheme("Idle Fingers")
 
 }
